0892576b62528fc428db07866bcc88f7e93cfd9b
Aaron Nguyen <aaronnguyen.me@outlook.com>
2021-02-04 12:12:10 -0500

    Reformat

--- Program.cs
+++ Program.cs
@@ -8,16 +8,16 @@ namespace RecursiveFizzBuzz
         {
             Console.WriteLine("RECURSIVE FIZZBUZZ");
             Console.WriteLine("------------------");
-            int number = ReadInt(min: 1, max: 9999, prompt:"INPUT: Enter a number: ");
+            int number = ReadInt(min: 1, max: 9999, prompt: "INPUT: Enter a number: ");
             DoRecursiveFizzBuzz(1, number);
         }
-        
+
         static int ReadInt(int min, int max, string prompt)
         {
             Console.Write(prompt);
             string resultString = Console.ReadLine();
             int result = int.Parse(resultString);
-            
+
             if ((result < min) || (result > max))
             {
                 Console.WriteLine("PROMPT: Please enter a number between 1 and 9999");
@@ -37,14 +37,14 @@ namespace RecursiveFizzBuzz
             }
             else
             {
-                string result="";
+                string result = "";
                 if (current % 3 == 0)
                     result += "Fizz";
                 if (current % 5 == 0)
                     result += "Buzz";
                 if (result == "")
                     result += current;
-                
+
                 Console.WriteLine(result);
 
                 DoRecursiveFizzBuzz(current + 1, max);
